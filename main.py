af71f0cfdd9ec6b127c92176fa5d08d32119c6ca
Aaron 110518 <110518@eesd.org>
2021-01-15 00:17:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print("Hello " + "World")
+# this is concatenation of strings
+print("This starts a new line")
+print()
+print("Tomorrow is Friday")
+
+# if adding strings is called "concatenation"
+# what is the + for integers called?
+
+print("5" + "4") # Concatenation
+print(5 + 4) # Addition
+
+firstName = "Aaron"
+print("My name is " + firstName)
+age = 13 # Integer
+print("My age is " + str(age))
+
+# firstName != FirstName
+
+userName = input("What is your name? ")
+print("Hey " + userName)
+
+#Ask the user for their birth year
+
+birthYear = input("What year were you born? ")
+print("Oh so you must be " + str(2021-int(birthYear)) + " years old")
